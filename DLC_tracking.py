03b8f03fd6d65bafe755c68f1ec49dec37744499
PIG-ZMS <pig.mingshuaizhu@gmail.com>
2024-09-18 01:34:45 +0100

    Update DLC_tracking.py

--- DLC_tracking.py
+++ DLC_tracking.py
@@ -14,14 +14,40 @@ parameter = {
     'tracking_file_sufix': '.csv',
     'tracking_file_tag': 'DLC',
     'well_coord': [[30,240],[250,240]],
-    'detecting_radius': 20
+    'detecting_radius': 10,
+    'point_of_no_return': 1,
+    'CamFs':16,
+    'sig_level':0.95
     }
 
 class frame:
-    def __init__(self,head,shoulder,bottom):
+    def __init__(self,head,shoulder,bottom,parameter):
         self.head = head
         self.shoulder = shoulder
         self.bottom = bottom
+        self.well1 = parameter['well_coord'][0]
+        self.well2 = parameter['well_coord'][1]
+        self.r = parameter['detecting_radius']
+    
+    def IsCloseToWell(self):
+        if Dis(self.well1,self.well2)<=self.r:
+            print('Error,the two wells are too close!')
+            return
+        if Dis(self.head,self.well1)<=self.r:
+            self.well = self.well1
+            self.well_tag = 1
+            return True
+        if Dis(self.head,self.well2)<=self.r:
+            self.well = self.well2
+            self.well_tag = 2
+            return True
+        return False
+    
+    def IsInward(self):
+        if Dis(self.head,self.well)<=Dis(self.shoulder,self.well):
+            return True
+        else:
+            return False
 
 class trace:
     def __init__(self,file,ID,day,parameter):
@@ -51,15 +77,46 @@ class trace:
                         bottom_x = file.iloc[i,j]
                     if (file.iloc[self.xy_index,j]=='y'):
                         bottom_y = file.iloc[i,j] 
-            single_frame = frame([head_x,head_y],[shoulder_x,shoulder_y],[bottom_x,bottom_y])
+            single_frame = frame([head_x,head_y],[shoulder_x,shoulder_y],[bottom_x,bottom_y],parameter)
             self.frames.append(single_frame)
         
-    def MouseRadar (parameter):
-        #distance to well 1
-        well1_coord = parameter['well_coord'][0]
-        well2_coord = parameter['well_coord'][1]
+    def Marking (self,parameter):
+        current_state = 'Outside'
+        self.BoarderForce = {
+            'Enter':[],
+            'Leave':[],
+            'Well':[]
+            }
         
-        return
+        for i in range (len(self.frames)):
+            frame = self.frames[i]
+            #This refers that the mouse may trying to reach the well
+            if (frame.IsCloseToWell() and current_state == 'Outside'):
+                if self.BoarderPass(parameter,i,True):
+                    current_state = 'Inside'
+                    self.BoarderForce['Leave'].append(i/parameter['CamFs'])
+                    self.BoarderForce['well'].append(frame.well_tag)
+            if (not (frame.IsCloseToWell()) and current_state == 'Inside'):
+                if self.BoarderPass(parameter,i,False):
+                    current_state = 'Outside'
+                    self.BoarderForce['Leave'].append(i/parameter['CamFs'])
+                    
+    
+    def BoarderPass (self,parameter,index,expectation):
+        frame_of_no_return = round(parameter['point_of_no_return']*parameter['CamFs'])
+        legit = 0
+        outlaw = 0
+        for i in self.frames[index:max(len(self.frame),index+frame_of_no_return)]:
+            if (self.frames[i].IsCloseToWell() == expectation):
+                legit+=1
+            else:
+                outlaw+=1
+        ratio = legit/(legit+outlaw)
+        if ratio >= parameter['sig_level']:
+            return True
+        else:
+            return False
+        
         
 class dayx:
     def __init__(self,parameter,folder,day):
